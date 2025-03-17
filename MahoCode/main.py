860cec9bff950f5828e2a9958aaec6ba8ed86729
Mohammad Javad <66680750+ShoghShahadat@users.noreply.github.com>
2025-03-17 06:43:38 +0330

    Update main.py
    Signed-off-by: Mohammad Javad <66680750+ShoghShahadat@users.noreply.github.com>

--- MahoCode/main.py
+++ MahoCode/main.py
@@ -101,4 +101,4 @@ def restore_version(version):
 
 if __name__ == '__main__':
     webbrowser.open("http://127.0.0.1:8283")
-    app.run(debug=True, host='0.0.0.0', port=8283)
+    app.run(debug=False, host='0.0.0.0', port=8283)
