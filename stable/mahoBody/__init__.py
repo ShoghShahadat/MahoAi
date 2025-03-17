d65e0668747df1a2b84c16a330570856032292ee
Mohammad Javad <66680750+ShoghShahadat@users.noreply.github.com>
2025-03-17 04:21:36 +0330

    Delete stable directory
    Signed-off-by: Mohammad Javad <66680750+ShoghShahadat@users.noreply.github.com>

--- stable/mahoBody/__init__.py
+++
@@ -1,4 +0,0 @@
-from .func import payload,read_directory_path,write_directory_path
-from .set_json import process_json
-
-__all__ = ["payload","read_directory_path","write_directory_path","process_json"]
